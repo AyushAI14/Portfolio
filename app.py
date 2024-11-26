aa05063e7b1716e8780790d7a45e565b30f2ec38
AyushAI14 <ayushaiml14@gmail.com>
2024-11-26 08:51:51 +0530

    Made a navbar

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 
 app = Flask(__name__,template_folder='templates',static_folder='static',static_url_path='/')
 @app.route('/')
 def home():
     return render_template('Index.html')
+if __name__ == '__main__':
+    app.run(port=5000 , debug=True)
+
+    
